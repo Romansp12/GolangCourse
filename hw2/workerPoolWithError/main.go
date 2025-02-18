7466057b0ba2a1a32f9170e11e90a40a7767ac8b
Roman Spirkin <romansp12@mail.ru>
2025-02-18 20:34:12 +0300

    fix

--- hw2/workerPoolWithError/main.go
+++ hw2/workerPoolWithError/main.go
@@ -3,32 +3,46 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
+	"sync/atomic"
+	"time"
+
+	"math/rand"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-func Launch(n,m int) (input chan Task, resultErr chan error) {
+// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
+func Run(tasks []Task, n, m int) error {
 	ctx,cancel := context.WithCancel(context.Background())
 	
-	outputTask:=make(chan error,n)
-	input=make(chan Task,n)
-	resultErr=make(chan error,1)
+	cntTask:=len(tasks)
 	
+	outputTask:=make(chan error,n)
+	input:=make(chan Task)
+	resultErr:=make(chan error,1)
+
 	go func(){
 		for e :=range outputTask{
+			fmt.Println("receved error",e)
 			if e!=nil{
 				if m>=1{
 					m--
 				}else{
 					cancel()
 					resultErr<-ErrErrorsLimitExceeded
+					close(resultErr)
+					return
 				}
 			}
-			n--
-			if n==0{
+			cntTask--
+			if cntTask==0{
+				cancel()
 				resultErr<-nil
+				close(resultErr)
+				return
 			}
 		}
 	}()
@@ -38,22 +52,62 @@ func Launch(n,m int) (input chan Task, resultErr chan error) {
 			for{
 				select{
 				case <-ctx.Done():
-					return
-				case task:=<-input:
-					outputTask<-task()
+					return 
+				default:
+					task,closed:=<-input
+					if !closed{
+						return
+					}
+					select{
+					case <-ctx.Done():
+						return
+					default:
+						outputTask<-task()
+					}
 				}
 			}
 		}()
 	}
-	
-	return input,resultErr
+
+
+	go func(){
+		for _, task := range tasks {
+			select{
+			case <-ctx.Done():
+				return
+			default:
+				select{
+				case <-ctx.Done():
+					return
+				case input<-task:
+				}
+			}
+		}
+		close(input)
+	}()
+
+	return <-resultErr
 }
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
-	input,resultErr := Launch(n,m)
-	for _, task := range tasks {
-		input<-task
-	}
-	return <-resultErr
+func main() {
+  tasksCount := 50
+  tasks := make([]Task, 0, tasksCount)
+
+  var runTasksCount int32
+
+  for i := 0; i < tasksCount; i++ {
+   err := fmt.Errorf("error from task %d", i)
+   tasks = append(tasks, func() error {
+	fmt.Printf("task start %d \n",i)
+    time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+    atomic.AddInt32(&runTasksCount, 1)
+    return err
+   })
+  }
+
+  workersCount := 10
+  maxErrorsCount := 23
+  err := Run(tasks, workersCount, maxErrorsCount)
+  fmt.Println(runTasksCount)
+  fmt.Println(err)
 }
