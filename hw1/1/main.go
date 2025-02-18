83fd65527c3190b67bf3a38d50f8a6dc7bde2579
Roman Spirkin <romansp12@mail.ru>
2025-02-18 21:21:14 +0300

    fix

--- hw1/1/main.go
+++ hw1/1/main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 )
 
-func calculate(i int,n int,res chan float64) {
+func calculate(ctx context.Context,i int,n int,res chan float64) {
 	k:=i
 	for {
-		res<-(float64(2.0)/(float64(4*k+1)*float64(4*k+3)))
-		k+=n
+		select {
+		case <-ctx.Done():
+			return
+		case res<-(float64(2.0)/(float64(4*k+1)*float64(4*k+3))):
+			k+=n
+		}
 	}
 }
 
@@ -26,21 +32,44 @@ func main() {
 		return
 	}
 	
-	res:=make(chan float64,n)
+	ctx,cancel:=context.WithCancel(context.Background())
+	res:=make(chan float64)
 	var ans float64
+	
+	wg:=sync.WaitGroup{}
+	wg.Add(1)
 	go func(){
-		for x:= range res{
-			ans +=x
+		defer wg.Done()
+		for {
+		select{
+			case <-ctx.Done():
+				return
+			default:
+				x,closed:=<-res
+				if !closed{
+					return
+				}
+				select{
+				case <-ctx.Done():
+					return
+				default:
+					ans+=x
+				}
+		}
 		}
 	}()
 	
 	for i:=0;i<n;i++ {
-		go calculate(i,n,res)
+		go calculate(ctx,i,n,res)
 	}
 	fmt.Println("Running gorutines for counting: ",n)
 	
 	exit := make(chan os.Signal, 1)
 	signal.Notify(exit, os.Interrupt, syscall.SIGTERM)
 	<-exit
+	
+	cancel()
+	wg.Wait()
+	
 	fmt.Println(ans*4)
 }
