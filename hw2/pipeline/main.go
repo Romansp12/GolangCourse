fc4e115e9e34de3f2543ef53ee1469384afe7680
Roman Spirkin <romansp12@mail.ru>
2025-02-18 23:19:19 +0300

    fix

--- hw2/pipeline/main.go
+++ hw2/pipeline/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -10,12 +15,63 @@ type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	curIn:=in
-	for _,stage := range stages {	
-		go func () {
-			curIn=stage(curIn)
-		}()
+	for _,stage := range stages {
+		// перед каждой стадией поставим фильтр который перед отправкой, будет проверять не отменено ли исполенение
+		// CurIn filtered to res
+		res:=make(chan interface{})
+		go func(curChan In){
+			for{
+				select {
+				case <-done:
+					return
+				default:
+					x,clse:=<-curChan
+					if !clse{
+						return
+					}
+					select{
+					case <-done:
+						return
+					default:
+						res<-x
+					}
+				}
+			}
+		}(curIn)
+		curIn=res
+		curIn=stage(curIn)
 	} 
 	return curIn
+}
+
+func main() {
+	stages:=make([]Stage,2)
+	stages[0]=func(in In) (out Out) {
+		res:=make(chan interface{})
+		go func(){
+			x:=<-in
+			fmt.Println("stages 0",x)
+			res<-x
+		}()
+		return res
+	}
+
+	stages[1]=func(in In) (out Out) {
+		res:=make(chan interface{})
+		go func(){
+			x:=<-in
+			fmt.Println("stages 1",x)
+			res<-x
+		}()
+		return res
+	}
+
+	input:=make(chan interface{})
+	done:=make(chan interface{})
+	output:=ExecutePipeline(input,done,stages...)
+	input<-123
+	fmt.Println(<-output)
+	time.Sleep(time.Second)
 }
 
 
